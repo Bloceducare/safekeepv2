06e65f826ed1cda4a0a3d0ce5036735a66ce54ae
okeken <okekehinde@gmail.com>
2023-03-27 14:34:39 +0100

    settings page

--- src/pages/dashboard/settings.tsx
+++ src/pages/dashboard/settings.tsx
@@ -1,14 +1,94 @@
 import DashboardLayout from "@components/Layout/dashboard";
+import Image from "@components/primitives/image";
+import Select, { SelectOption } from "@components/primitives/select";
 
 const Settings = () => {
   return (
     <div className="w-full">
       <div className="bg-safekeep-white p-6">
-        <div className="text-safekeep-blue-500">General</div>
-        <div className="flex justify-between">
+        <div className="text-safekeep-blue-500 text-lg mb-6 font-paralucentMedium" >General</div>
+        <div className="pl-2 font-polySansTrial">
+        <div className="flex mb-8 justify-between">
           <div>Basic Currency</div>
-          <div>Two</div>
+          <div>
+            US
+            {/* <Select name="lang">
+              <div>US</div>
+            </Select> */}
+          </div>
         </div>
+
+        <div className="flex justify-between mb-8">
+          <div>App Theme</div>
+          <div>
+            <Image src="/toggle-light.svg" width={40} height={40} />
+            
+</div>
+        </div>
+
+
+        <div className="flex justify-between mb-6">
+          <div>Language</div>
+          <div>
+            <Select name="lang">
+              <SelectOption value="eng"> 
+                <div>English</div>
+              </SelectOption>
+
+            </Select>
+            
+</div>
+        </div>
+        </div>
+
+      </div>
+
+
+      <div className="lg:grid grid-cols-10 flex justify-center mt-12 gap-4">
+
+        <div className="col-span-4 bg-safekeep-white p-5" >
+          <div className="mb-8 text-lg text-safekeep-blue-500 font-paralucentMedium">
+            Privacy
+          </div>
+         <div className="pl-2 font-polySansTrial">
+         <div className="mb-8">
+            Privacy Policy
+          </div>
+          <div className="mb-8">
+            Terms of Service
+          </div>
+          <div className="mb-8">
+            Partners
+          </div>
+         </div>
+          </div>
+          <div className="col-span-6 bg-safekeep-white p-5" >
+          
+          <div className="mb-8 text-lg text-safekeep-blue-500 font-paralucentMedium">
+            Company
+          </div>
+         <div className="pl-2 font-polySansTrial">
+         <div className="mb-8">
+           Whitepaper
+          </div>
+          <div className="mb-8">
+           Careers
+          </div>
+          <div className="mb-8 flex justify-between">
+          <div>
+            Follow Us
+          </div>
+          <div>
+            Twi
+          </div>
+          <div>
+            Github
+          </div>
+          </div>
+         </div>
+          
+          </div>
+
       </div>
     </div>
   );
